b709923cdd33ab33333728c34c0ba62fb4991b4e
Rafal Opiela <opiela@tlen.pl>
2023-03-31 10:15:27 +0200

    Updating nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    BACKEND_URL: process.env.BACKEND_URL
+  runtimeConfig: {
+    public: {
+      BACKEND_URL: process.env.BACKEND_URL
+    }
   },
 })
