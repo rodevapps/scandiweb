b8f962a1183002c0d9c1da589ab930975feb5e4d
Rafal Opiela <opiela@tlen.pl>
2023-04-01 23:28:07 +0200

    Updating configuration

--- backend/databases/Database.php
+++ backend/databases/Database.php
@@ -19,12 +19,12 @@ abstract class Database implements IDatabase
 
     protected function seed() {
         try {
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('aaa', 'Product 1', '10.00', 'dvd', '1500', NULL, NULL, NULL, NULL);");
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('bbb', 'Product 2', '200.00', 'furniture', NULL, NULL, '122', '37.5', '154.1');");
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('ccc', 'Product 3', '50.60', 'book', NULL, '1.36', NULL, NULL, NULL);");
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('ddd', 'Product 4', '20.05', 'dvd', '900', NULL, NULL, NULL, NULL);");
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('eee', 'Product 5', '400.00', 'furniture', NULL, NULL, '554', '77.5', '334.1');");
-            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('fff', 'Product 6', '70.60', 'book', NULL, '0.9', NULL, NULL, NULL);");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('aaa', 'NameTest000', '10.00', 'dvd', '1500', NULL, NULL, NULL, NULL);");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('bbb', 'NameTest001', '200.00', 'furniture', NULL, NULL, '122', '37.5', '154.1');");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('ccc', 'NameTest002', '50.60', 'book', NULL, '1.36', NULL, NULL, NULL);");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('ddd', 'NameTest003', '20.05', 'dvd', '900', NULL, NULL, NULL, NULL);");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('eee', 'NameTest004', '400.00', 'furniture', NULL, NULL, '554', '77.5', '334.1');");
+            $this->_connection->exec("INSERT INTO products (sku, name, price, product_type, size, weight, height, width, length) VALUES ('fff', 'NameTest005', '70.60', 'book', NULL, '0.9', NULL, NULL, NULL);");
         } catch(Exception $e) {
             throw new Exception($e->getMessage());
         }            
