e9844431f87f0ac22d728b37f626c9cc60d373b9
Rafal Opiela <rafal@server1.koperska.ddns.net>
2023-04-08 15:48:50 +0200

    Replacing localtunnel => pagekite

--- watchdog.sh
+++ watchdog.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 
-if [ `ps ax | egrep "lt --port" | wc -l` -lt 2 ]
+#if [ `ps ax | egrep "lt --port" | wc -l` -lt 2 ]
+#then
+ #export PATH="${PATH}:/home/rafal/node-v18.15.0-linux-x64/bin"
+ #export LD_LIBRARY_PATH="${PATH}:/home/rafal/node-v18.15.0-linux-x64/lib"
+ #nohup nice -n 19 lt --port 8000 -s scandiweb &> /home/rafal/scandiweb.log &
+#fi
+
+if [ `ps ax | egrep "pagekite.py" | wc -l` -lt 2 ]
 then
- export PATH="${PATH}:/home/rafal/node-v18.15.0-linux-x64/bin"
- export LD_LIBRARY_PATH="${PATH}:/home/rafal/node-v18.15.0-linux-x64/lib"
- nohup nice -n 19 lt --port 8000 -s scandiweb &> /home/rafal/scandiweb.log &
+ cd `dirname ${0}`
+
+ python3 pagekite.py 8000 scandiweb2.pagekite.me
 fi
 
 
